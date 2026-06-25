a4fa5016df841ddf4019e77be2b552743b3147df
alok3989 <159879010+alok3989@users.noreply.github.com>
2026-06-25 13:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (93.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,55 @@
 class Solution {
-        public int[] searchRange(int[] nums, int target) {
+
+    public int[] searchRange(int[] nums, int target) {
+
         int[] ans = {-1, -1};
 
-        
-        //find first position
-        int start = search(nums, target, true);
-        //find last position
-        int end = search(nums, target, false);
-        ans[0] = start;
-        ans[1] = end;
+        // find first position
+        ans[0] = search(nums, target, true);
+
+        // find last position
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+
         return ans;
     }
 
-     //this function just returns the index value of target
-     int search(int[] nums, int target, boolean findStartIndex)
-     {
+    // returns index of target
+    int search(int[] nums, int target, boolean findStartIndex) {
+
         int ans = -1;
         int start = 0;
         int end = nums.length - 1;
 
-        while(start <= end)
-        {
+        while (start <= end) {
+
             int mid = start + (end - start) / 2;
 
-            if(target < nums[mid])
-            {
+            if (target < nums[mid]) {
                 end = mid - 1;
             }
-            else if(target > nums[mid])
-            {
+
+            else if (target > nums[mid]) {
                 start = mid + 1;
             }
-            else
-            {
-                //potential ans found
+
+            else {
+
                 ans = mid;
-                if(findStartIndex)
-                {
+
+                // move left
+                if (findStartIndex) {
                     end = mid - 1;
                 }
-                else
-                {
+
+                // move right
+                else {
                     start = mid + 1;
                 }
             }
         }
 
         return ans;
-     }
+    }
 }
