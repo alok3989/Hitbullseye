f0279b6370e24128fbdbed28cbb3e56d117fc7ef
alok3989 <159879010+alok3989@users.noreply.github.com>
2026-07-18 11:14:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (5.38%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                // You are in the decreasing part of the mountain
+                // This may be the answer, but look at left
+                end = mid;
+            } else {
+                // You are in the ascending part of the mountain
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+        // In the end, start == end and pointing to the largest number because of the above two checks
+        return start; // or return end as both are equal
+    }
+}
